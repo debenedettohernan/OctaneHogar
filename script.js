7f97f92ffe631c1d9d5783043e85355f56aec33d
debenedettohernan <hernan.debenedetto@hotmail.com>
2025-04-12 12:16:26 -0300

    se acomodan las tarjetas al apretar el boton ver mas en muebles industriales, ahora es responsive y esta bien distribuidas las tarjetas

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-    
-  function mostrarSeccion(id) {
-    const secciones = document.querySelectorAll('.seccion');
-    secciones.forEach(seccion => {
+function mostrarSeccion(id) {
+  const secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(seccion => {
       seccion.classList.remove('active');
-    });
-    const seccionActiva = document.getElementById(id);
-    if (seccionActiva) seccionActiva.classList.add('active');
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    mostrarSeccion('quienes'); // Mostrar "quienes" al cargar
-  
-    // Mostrar modal a los 5 segundos
-    setTimeout(() => {
-      mostrarModal();
-    }, 5000);
   });
-  
+  const seccionActiva = document.getElementById(id);
+  if (seccionActiva) {
+      seccionActiva.classList.add('active');
+      // Agregar la entrada al historial
+      history.pushState({ section: id }, '', `#${id}`);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarSeccion('quienes'); // Mostrar "quienes" al cargar
+
+  // Mostrar modal a los 5 segundos
+  setTimeout(() => {
+      mostrarModal();
+  }, 5000);
+});
+
+// Manejar el evento popstate
+window.addEventListener('popstate', (event) => {
+  if (event.state) {
+      mostrarSeccion(event.state.section);
+  }
+});
